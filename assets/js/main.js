f7404b30b71c8c45f057231763088e148a723873
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-09-09 21:51:25 -0300

    create content

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/*==================== LETRAS ANIMEJS  ==================== */
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
+
+ 
+
+  /*==================== MOUSE OVER IMGS ==================== */
+let test = document.getElementById("cardone");
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", function( event ) {
+  // highlight the mouseenter target
+  event.target.src = "assets/img/tiny.jpg";
+  event.target.style.scale = "1.1";
+
+  
+}, false);
+
+test.addEventListener("mouseout", function( event ) {
+  // highlight the mouseout target
+  event.target.src = "assets/img/tiny.jpg";
+  event.target.style.scale = "1";})
+  
